f4ce28725faebfb019f1fc67a2f107d08a282d6e
bilelhamitouche <bilelhamitouche4@gmail.com>
2024-04-16 23:06:16 +0100

    Finishing up the hash map data structure.

--- hashmaps.js
+++ hashmaps.js
@@ -1,6 +1,13 @@
+import { LinkedList, Node } from "./linkedList.js";
+
 class HashMap {
   constructor() {
-    this.map = [];
+    this.capacity = 16;
+    this.loadFactor = 0.75;
+    this.buckets = [];
+    for (let i = 0; i < this.capacity; i++) {
+      this.buckets[i] = new LinkedList();
+    }
   }
   hash(key) {
     let hashCode = 0;
@@ -10,22 +17,102 @@ class HashMap {
     }
     return hashCode;
   }
+  set(key, value) {
+    let index = this.hash(key) % 16;
+    if (index < 0 || index >= this.capacity) {
+      throw new Error("Trying to access index out of bound");
+    }
+    let current = this.buckets[index].head;
+    if (this.buckets[index].contains(key)) {
+      for (let i = 0; i < this.buckets[index].find(key); i++) {
+        current = current.next;
+      }
+      current.value = value;
+    }
+    this.buckets[index].append(key, value);
+  }
 
-  set(key, value) {}
-
-  get(key) {}
-
-  has(key) {}
-
-  remove(key) {}
-
-  length() {}
-
-  clear() {}
-
-  keys() {}
-
-  values() {}
-
-  entries() {}
+  get(key) {
+    let index = this.hash(key) % 16;
+    if (!this.buckets[index].contains(key)) return null;
+    let current = this.buckets[index].head;
+    for (let i = 0; i < this.buckets[index].find(key); i++) {
+      current = current.next;
+    }
+    return current.value;
+  }
+  has(key) {
+    let index = this.hash(key) % 16;
+    return this.buckets[index].contains(key);
+  }
+  remove(key) {
+    let index = this.hash(key) % 16;
+    if (!this.buckets[index].contains(key)) return false;
+    this.buckets[index].removeAt(this.buckets[index].find(key));
+    return true;
+  }
+  length() {
+    const size = this.buckets.reduce((sum, current) => {
+      return (sum += current.size());
+    }, 0);
+    return size;
+  }
+  clear() {
+    this.buckets.forEach((item) => {
+      if (item.size()) {
+        for (let i = 0; i < item.size(); i++) {
+          item.head = null;
+        }
+      }
+    });
+  }
+  keys() {
+    const keysArray = this.buckets.reduce((res, current) => {
+      let currNode = current.head;
+      for (let i = 0; i < current.size(); i++) {
+        res.push(currNode.key);
+        currNode = currNode.next;
+      }
+      return res;
+    }, []);
+    return keysArray;
+  }
+  values() {
+    const valuesArray = this.buckets.reduce((res, current) => {
+      let currNode = current.head;
+      for (let i = 0; i < current.size(); i++) {
+        res.push(currNode.value);
+        currNode = currNode.next;
+      }
+      return res;
+    }, []);
+    return valuesArray;
+  }
+  entries() {
+    const keysArray = this.keys();
+    const valuesArray = this.values();
+    const entriesArray = [];
+    for (let i = 0; i < keysArray.length; i++) {
+      entriesArray[i] = { key: keysArray[i], value: valuesArray[i] };
+    }
+    return entriesArray;
+  }
 }
+
+const hashMap = new HashMap();
+console.log(hashMap.hash("Bilel"));
+console.log(hashMap.set("Bilel", "Hello world"));
+console.log(hashMap.set("Rachid", "world"));
+console.log(hashMap.set("Latifa", "Hi"));
+console.log(hashMap.set("Abdellah", "What's up?"));
+console.log(hashMap.set("Muhammad", "Hello guys"));
+console.log(hashMap);
+console.log(hashMap.get("Bilel"));
+console.log(hashMap.has("Bilel"));
+console.log(hashMap.remove("Bilel"));
+console.log(hashMap.keys());
+console.log(hashMap.values());
+console.log(hashMap.entries());
+console.log(hashMap.length());
+hashMap.clear();
+console.log(hashMap);
---
+++ linkedList.js
@@ -0,0 +1,129 @@
+class Node {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  append(key, value) {
+    let curr = this.head;
+    let newNode = new Node(key, value);
+    if (curr === null) {
+      this.head = newNode;
+    }
+    while (curr) {
+      if (curr.next === null) {
+        curr.next = newNode;
+        newNode.next = null;
+      }
+      curr = curr.next;
+    }
+  }
+  prepend(key, value) {
+    let curr = this.head;
+    let newNode = new Node(key, value);
+    if (curr === null) {
+      this.head = newNode;
+      newNode.next = curr;
+    }
+    newNode.next = curr;
+    this.head = newNode;
+  }
+  size() {
+    let curr = this.head;
+    let length = 0;
+    if (curr === null) return 0;
+    while (curr) {
+      length++;
+      curr = curr.next;
+    }
+    return length;
+  }
+  getHead() {
+    return this.head;
+  }
+  getTail() {
+    let curr = this.head;
+    while (curr.next) {
+      curr = curr.next;
+    }
+    return curr;
+  }
+  at(index) {
+    let curr = this.head;
+    for (let i = 0; i < index && curr; i++) {
+      curr = curr.next;
+    }
+    return curr;
+  }
+  pop() {
+    let curr = this.head;
+    while (curr.next.next) {
+      curr = curr.next;
+    }
+    curr.next = null;
+  }
+  contains(key) {
+    let curr = this.head;
+    while (curr) {
+      if (curr.key === key) {
+        return true;
+      }
+      curr = curr.next;
+    }
+    return false;
+  }
+  find(key) {
+    let curr = this.head;
+    let counter = 0;
+    if (curr === null) return null;
+    while (curr) {
+      if (curr.key === key) return counter;
+      counter++;
+      curr = curr.next;
+    }
+  }
+  toString() {
+    let curr = this.head;
+    while (curr) {
+      console.log(`( ${curr.key}, ${curr.value} ) ->`);
+      curr = curr.next;
+    }
+    console.log(null);
+  }
+  insertAt(key, value, index) {
+    let curr = this.head;
+    let newNode = new Node(key, value);
+    if (index === 0) {
+      this.prepend(key, value);
+    } else if (index === this.size()) {
+      this.append(key, value);
+    } else {
+      for (let i = 1; i < index - 1 && curr; i++) {
+        curr = curr.next;
+      }
+      newNode.next = curr.next;
+      curr.next = newNode;
+    }
+  }
+  removeAt(index) {
+    let curr = this.head;
+    if (index === 0) {
+      this.head = curr.next;
+    } else if (index === this.size() - 1) {
+      this.pop();
+    } else {
+      for (let i = 1; i < index - 1 && curr.next; i++) {
+        curr = curr.next;
+      }
+      curr.next = curr.next.next;
+    }
+  }
+}
+
+export { LinkedList, Node };
